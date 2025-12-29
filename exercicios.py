33c33285c5f7b51b9032195be36de6b6b81fd57b
robcabr <132093163+robcabr@users.noreply.github.com>
2025-12-29 16:21:14 -0300

    adicionado exercicio 01 a 10 : ''

---
+++ exercicios.py
@@ -0,0 +1,118 @@
+# Inteiros
+
+#%%
+# Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+from docutils.utils.math.latex2mathml import modulo_functions
+
+primeiro_numero = int(input('Digite o seu primeiro numero: '))
+
+segundo_numero = int(input('Digite o seu segundo numero: '))
+
+soma = primeiro_numero + segundo_numero
+
+print(f'O valor total de {primeiro_numero} + {segundo_numero} é {soma}')
+
+
+#%%
+# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5. 
+
+numero_modulo = int(input("Digite o seu número: "))
+
+modulo = numero_modulo % 5
+
+print(f'O resto de {numero_modulo} por 5 é {modulo}')
+
+#%%
+# Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+primeiro_multiplicacao = int(input('Digite o seu primeiro numero: '))
+
+segundo_multiplicacao = int(input('Digite o seu segundo numero: '))
+
+multiplicacao = primeiro_multiplicacao * segundo_multiplicacao
+
+print(f'O valor total de {primeiro_multiplicacao} X {segundo_multiplicacao} é {multiplicacao}')
+
+#%%
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+primeiro_divisao = int(input('Digite o seu primeiro numero: '))
+
+segundo_divisao = int(input('Digite o seu segundo numero: '))
+
+divisao_inteira = primeiro_divisao // segundo_divisao
+
+print(f'A divisao inteira de {primeiro_divisao} por {segundo_divisao} = {divisao_inteira}')
+
+
+#%%
+
+# Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+numero_quadrado = int(input("Insira o valor a ser elevado ao quadrado: "))
+
+quadrado = numero_quadrado **2
+
+print(f"{numero_quadrado} elevado ao quadrado é {quadrado}")
+
+#%%
+
+# Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+# %%
+
+# Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+primeiro_float = float(input('Primeiro numero a ser adicionado: '))
+
+segundo_float = float(input('Segundo numero a ser adicionado: '))
+
+soma_float = primeiro_float + segundo_float
+
+print(f'A soma de {primeiro_float} com {segundo_float} resulta em {soma_float}')
+
+# %%
+
+# Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+valor_media_primeiro = float(input('Valor a ser somado: '))
+
+valor_media_segundo = float(input('Valor a ser somado: '))
+
+media = (valor_media_primeiro + valor_media_segundo) / 2
+
+print(f'A média de {valor_media_primeiro} e {valor_media_segundo} é {media}')
+
+# %%
+
+# Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+temperatura_c = float(input('Temperatura em Celsius: '))
+
+fahrenheit = (temperatura_c * 1.8) + 32
+
+print(f'A conversao de {temperatura_c} para F resulta em {fahrenheit}')
+
+# %%
+
+#Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+raio = float(input('Adicione o raio: '))
+
+circulo_area = 3.14 * (raio**2)
+
+print(f'O raio de {raio} resulta em {circulo_area}')
+
+# %%
+
+# String
+
+# %%
+
+# Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+string = input("Adicione a string a ser transformada")
+
+string_maiuscula = 
+
+
